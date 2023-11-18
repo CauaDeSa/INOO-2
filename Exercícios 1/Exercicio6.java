5a695a88d67963eb0148185afdc6bc12550b865d
Cauã Rufino de Sá <127906505+CauaDeSa@users.noreply.github.com>
2023-11-18 12:11:09 -0300

    Correção Exercício 6

--- Exercícios 1/Exercicio6.java
+++ Exercícios 1/Exercicio6.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Exercicio6 {
     
     public static void main(String[] args) {
-        int i, j, n, count = 1, value = 1;
+        int lines, count = 1, value = 1;
         
         Scanner input = new Scanner(System.in);
-        n = input.nextInt();
+        lines = input.nextInt();
 
-        for(j = 0; j < n; j++){
+        for(int j = 0; j < lines; j++){
 
-            for(i = 0; i < count; i++){
+            for(int i = 0; i < count; i++){
                 
                 if(i == 0)
                     System.out.printf("%d", value++);
