ddbd9586dcb8f696f4396f87cef478237fe0866a
Cauã Rufino de Sá <127906505+CauaDeSa@users.noreply.github.com>
2023-11-20 16:16:14 -0300

    Correção Exercício 1

--- 1. Exercícios de Fixação/Exercicio1.java
+++ 1. Exercícios de Fixação/Exercicio1.java
@@ -2,7 +2,6 @@ public class Exercicio1 {
 
     public static void main(String[] args) {
         for (int i = 1; i < 11; i++) {
-
             System.out.printf("%2d° = %2d\n", i, i * 3);
         }        
     }
