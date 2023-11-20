926b25d7e397892ed2d2371e0dabffb664c0ed1f
Cauã Rufino de Sá <127906505+CauaDeSa@users.noreply.github.com>
2023-11-20 16:16:53 -0300

    Correção Exercício 3

--- 1. Exercícios de Fixação/Exercicio3.java
+++ 1. Exercícios de Fixação/Exercicio3.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Exercicio3 {
+    
     public static void main(String[] args) {
         double media = 0;
 
         for(int i = 0; i < 10; i++) {
-
             Scanner input = new Scanner(System.in);
             media += input.nextDouble();
         }
@@ -13,4 +13,3 @@ public class Exercicio3 {
         System.out.printf("Media: %.1lf\n", media / 10);
     }
 }
-
